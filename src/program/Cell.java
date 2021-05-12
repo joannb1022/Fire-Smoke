1f501c734e812967ac0d01e830b93acb899ee414
Pseudo-Swetoniusz <jbtokarska@gmail.com>
2021-05-12 11:45:21 +0200

    Initial commit 2

--- src/program/Cell.java
+++ src/program/Cell.java
@@ -1,4 +1,16 @@
 package program;
 
+import utils.CellType;
+
+import java.util.ArrayList;
+
 public class Cell {
+    CellType type;
+    ArrayList<Cell> neighbours;
+
+    public Cell() {
+        this.type = CellType.AVAIABLE;
+        this.neighbours = new ArrayList<>();
+    }
+
 }
---
+++ src/utils/CellType.java
@@ -0,0 +1,8 @@
+package utils;
+
+public enum CellType {
+    AVAIABLE,
+    BURNING,
+    BURNED,
+    NONFUEL
+}
