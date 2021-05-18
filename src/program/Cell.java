b2fe8e205664df0047516a07146b34c1dee086ee
joannb1022 <joaanna.bryk1022@gmail.com>
2021-05-18 19:18:04 +0200

    neighbours, fire

--- src/program/Cell.java
+++ src/program/Cell.java
@@ -9,6 +9,16 @@ public class Cell {
     private CellType type;
     private CellFuel fuel;
     private double temperature;
+    private double convection; //parametr alfa
+    private double conduction; //parametr lambda
+    private double energy;
+    private double burnTemp;
+
+    //masa i cieplo wlasciwe i czas palenia moze przydatny do wygaszenia
+    // private double mass;
+    // private double heatEnergy;
+    //private double burnTime;
+
     private ArrayList<Cell> neighbours;
 
     public Cell(double t, CellFuel f) {
@@ -22,4 +32,77 @@ public class Cell {
         this.neighbours.add(cell);
     }
 
+    public void setParameters(){
+      //wzielam te parametry z tej pracy
+      this.convection = 0.3;
+      this.conduction = 5;
+
+      if (this.fuel == CellFuel.TREE || this.fuel == CellFuel.GRASS){
+        this.burnTemp = 200; //w stopniach Celsjusza
+      }
+    }
+
+    public void checkState(){
+      if (this.temperature > this.burnTemp && this.type == CellType.AVAILABLE){
+        this.type = CellType.BURNING;
+      }
+
+      //warunek na wygaszenie
+      // if (this.type == BURNING){
+      //   this.type = BURNED;
+      // }
+
+    }
+
+    // public void updateTemperature(){
+    //
+    // }
+
+    //powietrze - powietrze (bierzemy komorke nad nami, Td to my)
+    public void Convection(/*Cell cell*/){
+        this.convection -=0.1; //trzeba ten zminiejszac
+        // this.convection = this.convection / (this.mass * this.heatEnergy);  //to jest wzor z tej pracy
+
+        for (Cell cell : this.neighbours){
+            //jesli jest nad nasza komorka
+            if (cell.getFuel() == CellFuel.AIR){
+                double neighTemp = cell.getTemperature();
+                this.temperature = this.temperature - this.convection*(this.temperature - neighTemp);
+                neighTemp  = neighTemp + this.convection*(this.temperature - neighTemp);
+                cell.setTemperature(neighTemp);
+            }
+        }
+
+    }
+
+    //drzewa - drzewa (wszystko oprocz powietrza mam na mysli)
+    // public void Conduction(){
+    //     for (Cell cell : this.neighbours){
+    //       if (cell.getFuel == TREE && cell.getTemperature < this.temperature){
+    //
+    //       }
+    //
+    //     }
+    //
+    // }
+
+    //drzewa itd - powietrze
+    public void Radiation(){
+
+    }
+
+    public void setTemperature(double temp){
+      this.temperature = temp;
+    }
+    public double getTemperature(){
+      return this.temperature;
+    }
+
+    public CellType getType(){
+      return this.type;
+    }
+
+    public CellFuel getFuel(){
+      return this.fuel;
+    }
 }
--- src/program/Environment.java
+++ src/program/Environment.java
@@ -23,6 +23,7 @@ public class Environment {
             for (int y = 0; y < this.depth; y++) {
                 for (int z = 0; z < this.height; z++) {
                     cells[x][y][z] = new Cell(this.temperature, CellFuel.AIR);
+                    cells[x][y][z].setParameters();
                 }
             }
         }
@@ -30,16 +31,56 @@ public class Environment {
         for (int x = 0; x < this.width; x++) {
             for (int y = 0; y < this.depth; y++) {
                 for (int z = 0; z < this.height; z++) {
-
+                    if (z + 1 < this.height){
+                        cells[x][y][z].addNeighbour(cells[x][y][z+1]);
+                    }
+                    if (z > 0){
+                        cells[x][y][z].addNeighbour(cells[x][y][z-1]);
+                    }
+                    if (x + 1 < this.width){
+                        cells[x][y][z].addNeighbour(cells[x+1][y][z]);
+                    }
+                    if (x > 0){
+                        cells[x][y][z].addNeighbour(cells[x-1][y][z]);
+                    }
+                    if (y + 1 < this.depth){
+                        cells[x][y][z].addNeighbour(cells[x][y+1][z]);
+                    }
+                    if (y > 0){
+                      cells[x][y][z].addNeighbour(cells[x][y-1][z]);
+                    }
                 }
-            }
-        }
+          }
+      }
+}
 
-    }
 
     public void iterate(){
+    //   for (int x = 0; x < this.width; x++) {
+    //       for (int y = 0; y < this.depth; y++) {
+    //           for (int z = 0; z < this.height; z++) {
+    //             cell[x][y][z].Conduction();
+    //           }
+    //         }
+    //       }
+    // for (int x = 0; x < this.width; x++) {
+    //     for (int y = 0; y < this.depth; y++) {
+    //         for (int z = 0; z < this.height; z++) {
+    //            cell[x][y][z].Convection();
+    //         }
+    //       }
+    //     }
+    //   }
 
-    }
+//   for (int x = 0; x < this.width; x++) {
+//       for (int y = 0; y < this.depth; y++) {
+//           for (int z = 0; z < this.height; z++) {
+//             cell[x][y][z].updateTemperature();
+//            }
+//      }
+// }
+
+}
 
     public int getDepth() {
         return this.depth;
